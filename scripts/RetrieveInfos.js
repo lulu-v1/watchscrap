180772de4806aba3e6a5c1147fdec2289e887e18
lulu-v2 <luca.izidi@gmail.com>
2023-12-21 19:16:39 +0100

    headless true

--- scripts/RetrieveInfos.js
+++ scripts/RetrieveInfos.js
@@ -11,7 +11,7 @@ async function RetrieveInfos() {
 
     try {
         for (let i = 0; i < watchModelIDs.length; i++) {
-            const watchModelID = watchModelIDs[i];
+            const watchModelID = watchModelIDs[i].value;
             console.log(`Scraping watch model ID: ${watchModelID}`);
             await page.goto(`https://www.chrono24.fr/search/index.htm?countryIds=FR&currencyId=EUR&dosearch=true&manufacturerIds=221&maxAgeInDays=0&models=${watchModelID}&pageSize=60&redirectToSearchIndex=true&resultview=block&sortorder=0`);
             await sleep(1000);
