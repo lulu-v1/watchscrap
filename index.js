a7b7a34f839929b04b312c1b221d13b95e51bbd9
DepressedNunu <110053182+DepressedNunu@users.noreply.github.com>
2023-12-21 18:01:58 +0100

    GetWactchModelsID

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-import puppeteer from 'puppeteer';
+import getWatchModelsID from "./scripts/WatchModelID.js";
 
+getWatchModelsID();
 
-(async () => {
-    let browser = await puppeteer.launch({ headless: false });
-    let page = await browser.newPage();
-    await page.goto('https://chrono24.fr/rolex/index.htm');
-    await page.waitForSelector('.js-cookie-accept-all');
-    await page.click('.js-cookie-accept-all');
-
-    function sleep(number) {
-        return new Promise(resolve => setTimeout(resolve, number));
-    }
-    //click on Model Button
-    await sleep(1000)
-    await page.waitForSelector('.select-like-button');
-    await sleep(1000)
-    const button = await page.$$('.select-like-button');
-    await button[1].click();
-    await page.evaluate(() => {
-        const data = document.querySelectorAll('pointer');
-
-        for (let i = 43; i < 100; i++) {
-            console.log(data[i])
-        }
-
-    })
-})();
---
+++ scripts/WatchModelID.js
@@ -0,0 +1,43 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs-extra';
+
+async function getWatchModelsID() {
+    let browser = await puppeteer.launch({headless: false});
+    let page = await browser.newPage();
+    await page.goto('https://chrono24.fr/rolex/index.htm');
+    await page.waitForSelector('.js-cookie-accept-all');
+    await page.click('.js-cookie-accept-all');
+
+    function sleep(number) {
+        return new Promise(resolve => setTimeout(resolve, number));
+    }
+
+    // Click on Model Button
+    await page.waitForSelector('.select-like-button');
+    const buttons = await page.$$('.select-like-button');
+    await buttons[1].click();
+    await sleep(500);
+    const watchModels = await page.evaluate(() => {
+        const dataElements = document.querySelectorAll('.filter-inputtype-checkbox.form-check.p-a-3.p-a-sm-0 > label > input');
+        const nameElements = document.querySelectorAll('.filter-inputtype-checkbox.form-check.p-a-3.p-a-sm-0 > label > span');
+
+        const watches = [];
+
+        for (let i = 0; i < dataElements.length; i++) {
+            const name = nameElements[i].innerText.replace(/\s\(\d+\)/, '');
+            const value = dataElements[i].value.trim();
+
+            watches.push({name, value});
+        }
+
+        return watches;
+    });
+
+    // Write to JSON file using fs-extra
+    await fs.ensureFile('./data/watchModelsID.json');
+    await fs.writeJson('./data/watchModelsID.json', watchModels, {spaces: 2});
+    await sleep(200)
+    await browser.close();
+}
+
+export default getWatchModelsID;
